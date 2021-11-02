f5cd82fa0788cecea8d03a88f9d48b0d73c4c019
Anton <anton.gusevv.01@gmail.com>
2021-11-02 13:14:53 +0300

    добавлены файлы проекта и dll библиотеки

---
+++ WPF_MVVM_Classes/WPF_MVVM_Classes/ViewModelBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF_MVVM_Classes
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        public void Dispose()
+        {
+            OnDispose();
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+
+        protected virtual void OnDispose()
+        {
+        }
+    }
+}
