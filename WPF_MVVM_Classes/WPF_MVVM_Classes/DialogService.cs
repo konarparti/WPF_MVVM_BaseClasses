2655a1521e21e87b0c8f6ea10906b915d99f9c91
Anton <anton.gusevv.01@gmail.com>
2022-02-15 22:42:55 +0300

    добавлены методы для сервиса диалога

---
+++ WPF_MVVM_Classes/WPF_MVVM_Classes/DialogService.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System.Windows;
+
+namespace WPF_MVVM_Classes
+{
+    public static class DialogService 
+    {
+        public static void ShowMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        public static bool OpenFileDialog(string filter, out string filePath)
+        {
+            bool isSucceeded;
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = filter
+            };
+            var dialogResult = openFileDialog.ShowDialog();
+            if (dialogResult is true)
+            {
+                filePath = openFileDialog.FileName;
+                isSucceeded = true;
+            }
+            else
+            {
+                filePath = string.Empty;
+                isSucceeded = false;
+            }
+            return isSucceeded;
+
+        }
+
+        public static bool SaveFileDialog(string filter, out string filePath)
+        {
+            bool isSucceeded;
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = filter
+            };
+            var dialogResult = saveFileDialog.ShowDialog();
+            if (dialogResult is true)
+            {
+                filePath = saveFileDialog.FileName;
+                isSucceeded = true;
+            }
+            else
+            {
+                filePath = string.Empty;
+                isSucceeded = false;
+            }
+            return isSucceeded;
+        }
+    }
+}
--- WPF_MVVM_Classes/WPF_MVVM_Classes/RelayCommand.cs
+++ WPF_MVVM_Classes/WPF_MVVM_Classes/RelayCommand.cs
@@ -18,8 +18,8 @@ namespace WPF_MVVM_Classes
 
         public event EventHandler CanExecuteChanged
         {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
         }
 
 
